90f64bbe1d9a145f4d7bb2bbedcec40560a8a1ef
Egip Sinargo <egipsinargo123@gmail.com>
2026-06-08 23:16:01 +0700

    add aceternity ui

--- frontend/components/ui/background-beams-with-collision.tsx
+++ frontend/components/ui/background-beams-with-collision.tsx
@@ -71,8 +71,7 @@ export const BackgroundBeamsWithCollision = ({
       ref={parentRef}
       className={cn(
         "relative flex items-center w-full justify-center overflow-hidden",
-        // h-screen if you want bigger
-        className
+        className,
       )}
     >
       {children}
@@ -197,7 +196,7 @@ const CollisionMechanism = React.forwardRef<
         }}
         className={cn(
           "absolute left-0 top-20 m-auto h-14 w-px rounded-full bg-gradient-to-t from-indigo-500 via-purple-500 to-transparent",
-          beamOptions.className
+          beamOptions.className,
         )}
       />
       <AnimatePresence>
